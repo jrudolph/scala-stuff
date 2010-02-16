24ec0143221b6600a94453c478cfb1c2d7fc9cb3
Johannes Rudolph <johannes_rudolph@gmx.de>
2010-02-16 14:56:02 +0100

    more abstraction

--- telnet/src/main/scala/telnet-console.scala
+++ telnet/src/main/scala/telnet-console.scala
@@ -3,15 +3,18 @@ object Telnet {
   import nsc._
   import nsc.interpreter._
 
-  def main(args: Array[String]) {
+  def repl(reader: Interpreter => InteractiveReader = InteractiveReader.createDefault(_)) {
     val intLoop = new InterpreterLoop
     intLoop.settings = new Settings(Console.println)
     intLoop.settings.classpath.value = "lib/scala-library.jar:lib/scala-compiler.jar"
     intLoop.createInterpreter
-    intLoop.in = InteractiveReader.createDefault(intLoop.interpreter)
-    
+    intLoop.in = reader(intLoop.interpreter)
     
     intLoop.repl()
     intLoop.closeInterpreter
+  }
+
+  def main(args: Array[String]) {
+    repl()
   }
 }
